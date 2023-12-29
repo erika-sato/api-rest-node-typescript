b76a87998259292db6159d1932d415f178548ffb
erika sato <eerikasato@gmail.com>
2023-12-29 09:48:43 -0300

    feat: add folders structure

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
+import 'dotenv/config'
+import { router } from './routes'
 
 const server = express()
 
+server.use(express.json())
 
-
-server.get('/', (req, res) => {
-    return res.send('oi assi!')
-})
+server.use(router)
 
 export {server} 
